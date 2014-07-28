3ef9f45c92a701e32d2051d3d0a9c16cadbff693
Ryan Houck <ryan@levelskies.com>
2014-07-27 17:10:29 -0700

    fixed wsgi issue

--- gifts/wsgi.py
+++ gifts/wsgi.py
@@ -1,19 +1,15 @@
 """
-WSGI config for gifts project.
+WSGI config for traffic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-"""
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gifts.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-"""
-from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
 application = Cling(get_wsgi_application())
