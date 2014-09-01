2332f3fc7b1b27879e64f1073d933f72f5deb919
Ryan Houck <ryan@levelskies.com>
2014-09-01 16:39:46 -0700

    major changes to splash page

--- gifts/urls.py
+++ gifts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'gifts.views.splash', name='splash'),
+    url(r'^our-gifts/$', 'gifts.views.gifts', name='gifts'),
     url(r'^confirmation/(?P<ref>[A-Za-z0-9]{8})$', 'gifts.views.confirmation', name='confirmation'),
     (r'^django-rq/', include('django_rq.urls')),
 
